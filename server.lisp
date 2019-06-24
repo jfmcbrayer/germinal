250cb62a5ceca4f82ad3ff63912e0dbe76596eb3
Jason McBrayer <jmcbray@carcosa.net>
2019-06-24 15:14:29 -0400

    Basically working TLS echo server

--- server.lisp
+++ server.lisp
@@ -25,5 +25,24 @@
         (write-line line stream)
         (force-output stream)))))
 
+(defun tls-echo-handler (stream)
+  (echo-handler (cl+ssl:make-ssl-server-stream stream
+                                               :external-format '(:utf-8 :eol-style :lf)
+                                               :certificate "cert.pem"
+                                               :key "key.pem")))
+
 (defun start (&key (host "127.0.0.1") (port 1965))
-  (usocket:socket-server host port #'echo-handler))
+  (usocket:socket-server host port #'tls-echo-handler))
+
+(defun read-line-crlf (stream &optional eof-error-p)
+  (let ((s (make-string-output-stream)))
+    (loop
+      for empty = t then nil
+      for c = (read-char stream eof-error-p nil)
+      while (and c (not (eql c #\return)))
+      do
+         (unless (eql c #\newline)
+           (write-char c s))
+      finally
+         (return
+           (if empty nil (get-output-stream-string s))))))
