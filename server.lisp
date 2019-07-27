cbe7b96b39040d100e0c4e14e4fed79c4b9cc05f
Jason McBrayer <jmcbray@carcosa.net>
2019-07-27 11:34:11 -0400

    Only use TLS 1.3 (or higher, when that exists).
    I haven't tested that this change actually does what it says, only that it
    doesn't break anything.

--- server.lisp
+++ server.lisp
@@ -101,15 +101,19 @@
 
 (defun gemini-handler (stream)
   "The main Gemini request handler. Sets up TLS and sets up request and response"
-  (let* ((tls-stream
+  (let* ((*ssl-global-context
+           (make-context :disabled-protocols (list +ssl-op-no-sslv2+ +ssl-op-no-sslv3+
+                                                   +ssl-op-no-tlsv1+ +ssl-op-no-tlsv1-1+
+                                                   +ssl-op-no-tlsv1-2+)))
+         (tls-stream
            (make-ssl-server-stream stream
-                                          :certificate "cert.pem"
-                                          :key "key.pem"))
+                                   :certificate "cert.pem"
+                                   :key "key.pem"))
          (request (read-line-crlf tls-stream))
          (response (gemini-serve-file-or-directory request)))
     (write-sequence
      (babel:string-to-octets (str:concat (nth 0 response) '(#\return #\newline)))
-                    tls-stream)
+     tls-stream)
     (force-output tls-stream)
     (write-sequence (nth 1 response) tls-stream)
     (force-output tls-stream)))
